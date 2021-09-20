34ff3d9e7c4b6fb1cfdbf0af40bb1632b0935865
matthewcarpenter00 <matthew@firstlight.media>
2021-09-20 18:11:04 +0000

    random card project

--- src/app.js
+++ src/app.js
@@ -2,10 +2,41 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // call a function that will do the work
+  //   myfunction();
+  // };
+
+  // function myfunction() {
+  // create a function to generate random number
+  // create a function to generate random shape
+  // create a function to produce card with top two functions
+  // shape on bottom is upside down
+
+  //shapess ♦ ♥ ♠ ♣
+
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  let suits = ["&diams;", "&hearts;", "&spades;", "&clubs;"];
+
+  // random generators
+  let randomNumber = Math.floor(Math.random() * numbers.length);
+  let randomSuit = Math.floor(Math.random() * suits.length);
+
+  // connect the numbers and suits with the generators
+  let suit = suits[randomSuit];
+  let number = numbers[randomNumber];
+
+  // if statement if the random suit is heart or diamond  change the color to red
+  // use style.css.
+
+  if (suit == "&hearts;" || suit == "&diams;") {
+    document.querySelector(".card").style.color = "red";
+  } else {
+    document.querySelector(".card").style.color = "black";
+  }
+
+  // show the results in the html
+  document.querySelector(".top-shape").innerHTML = suit;
+  document.querySelector(".card-number").innerHTML = number;
+  document.querySelector(".bottom-shape").innerHTML = suit;
 };
